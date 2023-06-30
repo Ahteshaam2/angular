945291bce65357d5b3a558a10aae565aaf7cfa73
Ahteshaam2 <136442236+Ahteshaam2@users.noreply.github.com>
2023-06-30 09:54:08 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserinfoformComponent } from './userinfoform/userinfoform.component';
+import {FormsModule} from "@angular/forms";
+import { EmailformComponent } from './emailform/emailform.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    UserinfoformComponent,
+     EmailformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
